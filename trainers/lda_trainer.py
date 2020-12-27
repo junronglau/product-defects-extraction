497a6eaaa9748399dafedb07140007075dc467b4
Junrong Lau <junr0ng@hotmail.com>
2020-12-27 21:27:54 +0800

    Modified trainer to return scores for tracking experiments

--- trainers/lda_trainer.py
+++ trainers/lda_trainer.py
@@ -7,11 +7,13 @@ class LDATrainer:
         self.data = model.data
         self.dictionary = model.dictionary
         self.config = config
-        self.coherence_model = CoherenceModel(model=self.model, texts=self.data, dictionary=self.dictionary, coherence='u_mass')
+        self.coherence_model = CoherenceModel(model=self.model, corpus=self.data, dictionary=self.dictionary, coherence='u_mass')
 
     def evaluate(self):
         coherence_lda = self.coherence_model.get_coherence()
         print('Coherence Score: ', coherence_lda)
+        return coherence_lda
 
     def generate_topics(self):
-        self.model.print_topics()
+        for topic in self.model.print_topics(num_topics=10, num_words=10):
+            print(topic)
