d8101dc3aec785331ebbf98a3c7bdfab393be010
Junrong Lau <junr0ng@hotmail.com>
2020-12-27 21:26:49 +0800

    Loaded data on init

--- dataloader/data_loader.py
+++ dataloader/data_loader.py
@@ -4,10 +4,11 @@ import pandas as pd
 class DataLoader:
     def __init__(self, path):
         self.base_data_path = path.base_data_path
-        self.df = None
+        self.df = self.load_data(self.base_data_path)
 
-    def load_data(self):
-        self.df = pd.read_csv(self.base_data_path, encoding='utf-8')
+    @staticmethod
+    def load_data(path):
+        return pd.read_csv(path, encoding='utf-8')
 
     def get_data(self):
         return self.df
