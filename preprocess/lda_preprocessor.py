29057f5860c755a8d77a829da5243eb223f94bac
Junrong Lau <junr0ng@hotmail.com>
2021-01-02 19:25:13 +0800

    Updated class to inherit from base preprocessor class

--- preprocess/lda_preprocessor.py
+++ preprocess/lda_preprocessor.py
@@ -1,24 +1,25 @@
-from utils.preprocess import filter_reviews_rating
+from preprocess.base.labels_generator.base_preprocessor import BasePreprocessor
+
 from utils.preprocess import sent_to_words
 from utils.preprocess import generate_bigrams
 from utils.preprocess import generate_dictionary
 
 
-class LDAPreprocessor:
-    def __init__(self, config, reviews_df):
-        self.config = config
-        self.reviews_df = reviews_df
+class LDAPreprocessor(BasePreprocessor):
+    def __init__(self, df, config):
+        super(LDAPreprocessor, self).__init__(df, config)
         self.dictionary = None
         self.corpus = None
 
-    def preprocess_data(self):
-        self.reviews_df = filter_reviews_rating(self.reviews_df, 0.4)
-        split_words = list(sent_to_words(self.reviews_df['cleaned_text']))
-        bigrams = generate_bigrams(split_words)
+    def prepare_data(self):
+        self.preprocess_data()
+        self.format_data()
 
+    def format_data(self):
+        split_words = list(sent_to_words(self.df['cleaned_text']))
+        bigrams = generate_bigrams(split_words)
         # Create Dictionary
         self.dictionary = generate_dictionary(bigrams)
-
         # Term Document Frequency
         self.corpus = [self.dictionary.doc2bow(text) for text in bigrams]
 
