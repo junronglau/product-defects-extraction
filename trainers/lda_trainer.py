eac08aca33d8f635c646591969bc9278954ad2ed
Junrong Lau <junr0ng@hotmail.com>
2021-01-02 19:23:56 +0800

    Added method to generate text from dictionary

--- trainers/lda_trainer.py
+++ trainers/lda_trainer.py
@@ -2,12 +2,15 @@ from gensim.models import CoherenceModel
 
 
 class LDATrainer:
-    def __init__(self, config, model):
+    def __init__(self, model):
         self.model = model.model
         self.data = model.data
         self.dictionary = model.dictionary
-        self.config = config
-        self.coherence_model = CoherenceModel(model=self.model, corpus=self.data, dictionary=self.dictionary, coherence='u_mass')
+        self.coherence_model = CoherenceModel(model=self.model,
+                                              corpus=self.data,
+                                              texts=self.get_texts(),
+                                              dictionary=self.dictionary,
+                                              coherence='u_mass')
 
     def evaluate(self):
         coherence_lda = self.coherence_model.get_coherence()
@@ -17,3 +20,6 @@ class LDATrainer:
     def generate_topics(self):
         for topic in self.model.print_topics(num_topics=10, num_words=10):
             print(topic)
+
+    def get_texts(self):
+        return [[self.dictionary[word_id] for word_id, freq in doc] for doc in self.data]
