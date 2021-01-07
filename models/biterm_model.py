645c93cadff5a6311fb27477a6fbeb1a87203fe2
Junrong Lau <junr0ng@hotmail.com>
2021-01-07 15:56:20 +0800

    Added iterations attributes to Model class to standardize with other Model classes

--- models/biterm_model.py
+++ models/biterm_model.py
@@ -3,10 +3,11 @@ import pickle
 
 
 class BitermModel:
-    def __init__(self, config, dictionary):
+    def __init__(self, config, preprocessor):
         self.model_path = config.labels_generator.paths.save_model_path
-        self.dictionary = dictionary
+        self.dictionary = preprocessor.dictionary
         self.model = oBTM(num_topics=config.labels_generator.model.num_topics, V=self.dictionary)
+        self.iterations = config.labels_generator.model.iterations
 
     def save(self):
         with open(self.model_path, 'wb') as output:
