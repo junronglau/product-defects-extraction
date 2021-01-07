7f7093cc2adb0d56f78ccd1993fa34d45147aa22
Junrong Lau <junr0ng@hotmail.com>
2021-01-07 15:55:06 +0800

    Renamed evaluaton method to generate_topics

--- trainers/biterm_trainer.py
+++ trainers/biterm_trainer.py
@@ -1,14 +1,13 @@
 from biterm.utility import topic_summuary
 from tqdm import tqdm
-import numpy as np
 
 
 class BitermTrainer:
-    def __init__(self, config, model, corpus):
+    def __init__(self, model, corpus):
         self.model = model.model
         self.dictionary = model.dictionary
         self.corpus = corpus
-        self.iterations = config.labels_generator.model.iterations
+        self.iterations = model.iterations
         self.topics = None
 
     def train(self):
@@ -17,6 +16,5 @@ class BitermTrainer:
             self.model.fit(biterms_chunk, iterations=self.iterations)
         self.topics = self.model.transform(self.corpus)
 
-    def evaluate(self, data):
-        coherence_biterm = topic_summuary(self.model.phi_wz.T, data, self.dictionary, 10)
-        return np.mean(coherence_biterm.coherence)
+    def generate_topics(self, data):
+        topic_summuary(self.model.phi_wz.T, data, self.dictionary, 10)
