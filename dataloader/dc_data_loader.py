f35698650b5f347922182e3e450366982666ca5b
Junrong Lau <junr0ng@hotmail.com>
2021-01-26 01:31:21 +0800

    Change labels of test data during loading process

--- dataloader/dc_data_loader.py
+++ dataloader/dc_data_loader.py
@@ -7,7 +7,7 @@ class DataLoader:
         test_data_path = config.defects_classifier.paths.test_data_path
         generated_labels_path = config.labels_generator.paths.save_data_path
         self.common_cols = ['profile_link', 'comment', 'ASIN']
-        self.test_df = self.load_data(test_data_path)
+        self.test_df = self.load_testing_data(test_data_path)
         self.positive_labelled_df = self.load_data(generated_labels_path)
         self.base_df = self.load_data(base_data_path)
         self.train_df = self.load_training_data()
@@ -16,6 +16,11 @@ class DataLoader:
     @staticmethod
     def load_data(path):
         return pd.read_csv(path, encoding='utf-8')
+
+    def load_testing_data(self, path):
+        test_df = self.load_data(path)
+        test_df.loc[test_df['has_defect'] == 0, 'has_defect'] = -1
+        return test_df
 
     def load_training_data(self):
         pos_train_df = self.load_pos_training_data()
@@ -40,7 +45,7 @@ class DataLoader:
         """
         unlabelled_train_df = self._remove_duplicates(self.base_df, self.test_df, self.common_cols)
         unlabelled_train_df = self._remove_duplicates(unlabelled_train_df, self.positive_labelled_df, self.common_cols)
-        unlabelled_train_df['has_defect'] = 0
+        unlabelled_train_df['has_defect'] = -1
         return unlabelled_train_df
 
     @staticmethod
